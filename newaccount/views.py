a4d1659197c0c3da706065d5362fd3b060223c87
Nic Kiely <mitchell3141@gmail.com>
2015-11-15 01:42:37 -0500

    Implement backend newaccount form verification

--- newaccount/views.py
+++ newaccount/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.shortcuts import render
+import urllib
 import common.render
 from common.settings import get_page_config
 
@@ -19,9 +23,40 @@ def form(request):
 
 def validate(request):
     ''' Signup form validation handler '''
-    return JsonResponse({"status": "error", "message": "Test Case"})
+    msg = ''
+    if not('username' in request.GET):
+        msg = 'Name not given!'
+    elif not('email' in request.GET):
+        msg = 'Email not given!'
+    name = urllib.unquote(request.GET['username'])
+    email = urllib.unquote(request.GET['email'])
+    if not msg: msg = __validate_name__(name)
+    if not msg: msg = __validate_email__(email)
+
+    status = 'error' if msg else 'success'
+    return JsonResponse({"status": status, "message": msg})
 
 
 def submit(request):
     ''' Signup form submission handler '''
     return HttpResponse('')
+
+
+def __validate_name__(name):
+    ''' Internal validation function for username '''
+    lname = len(name)
+    if lname < 5:
+        return 'User name must be at least 5 characters long'
+    if lname > 64:
+        return 'User name must not be longer than 64 characters'
+    if len(User.objects.filter(username=name)):
+        return 'User name already in use'
+    return ''
+
+def __validate_email__(email):
+    ''' Internal validation function for email '''
+    try:
+        validate_email(email)
+    except ValidationError:
+        return 'Invalid email address: '+email
+    return ''
